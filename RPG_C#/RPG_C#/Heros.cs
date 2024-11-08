0f89519e27b6ddeea9f77086b1ca2de3a4df23f4
ImadBilS <imadbil.sghir78840@gmail.com>
2024-11-08 11:30:02 +0100

    Creation de la classe Heros
    Création de la classe héros avec ses caractéristiques

---
+++ RPG_C#/RPG_C#/Heros.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_C_
+{
+    internal class Heros
+    {
+        //Attributs
+        private int nbPv;
+        private int nbPvMax;
+        private int nbAttaque;
+        private int nbDefenseMax;
+        private int nbDefense;
+
+        //Propriétés
+        public Heros(int nbPv, int nbPvMax, int nbAttaque, int nbDefenseMax, int nbDefense)
+        {
+            this.nbPv = nbPv;
+            this.nbPvMax = nbPvMax;
+            this.nbAttaque = nbAttaque;
+            this.nbDefenseMax = nbDefenseMax;
+            this.nbDefense = nbDefense;
+        }
+
+
+        //Accesseurs
+        public int getNbPv()
+        {
+            return this.nbPv;
+        }
+        public int getNbPvMax()
+        {
+            return this.nbPvMax;
+        }
+        public int getNbAttaque()
+        {
+            return this.nbAttaque;
+        }
+        public int getNbDefense()
+        {
+            return this.nbDefense;
+        }
+        public int getNbDefenseMax()
+        {
+            return this.nbDefenseMax;
+        }
+        //Mutateurs
+        public void setNbPv(int nbPv)
+        {
+            this.nbPv = nbPv;
+        }
+        public void setNbPvMax(int nbPvMax)
+        {
+            this.nbPvMax = nbPvMax;
+        }
+        public void setNbAttaque(int nbAttaque)
+        {
+            this.nbAttaque = nbAttaque;
+        }
+        public void setNbDefense(int nbDefense)
+        {
+            this.nbDefense = nbDefense;
+        }
+        public void setNbDefenseMax(int nbDefenseMax)
+        {
+            this.nbDefenseMax = nbDefenseMax;
+        }
+
+        //Méthodes
+        //public void attaquer(heros h)
+        //{
+
+        // }
+    }
+}
